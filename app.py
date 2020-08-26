b2baf50f0c7c5dc0c82638081a0d19fbdd2b2f0a
adityamudgil2505 <adityamudgil2505@gmail.com>
2020-08-26 19:54:20 +0530

    Modify id to emails

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ def getting_user_id():
         with sql.connect('database.db') as con:
             con.row_factory = dict_factory
             curr = con.cursor()
-            curr.execute("SELECT user_id FROM users")
+            curr.execute("SELECT email_id FROM users")
             rows = curr.fetchall()
-            rows = [ x['user_id'] for x in rows]
+            rows = [ x['email_id'] for x in rows]
     except Exception as ex:
             con.rollback()
             print(ex)
@@ -29,25 +29,25 @@ def getting_user_id():
     con.close()
 
 def candidates_from_interview_id(interview_id):
-    cmd = "SELECT DISTINCT(T1.email_id) FROM users as T1 INNER JOIN candidates as T2 on T1.user_id = T2.candidate_id WHERE T2.interview_id = {}".format(interview_id)
+    cmd = "SELECT DISTINCT(T1.candidate_id) FROM candidates as T1 WHERE T1.interview_id = {}".format(interview_id)
     candidates = []
     with sql.connect('database.db') as con:
         con.row_factory = dict_factory
         curr = con.cursor()
         curr.execute(cmd)
         rows = curr.fetchall()
-        candidates = [ x['email_id'] for x in rows]
-        print(candidates)
+        candidates = [ x['candidate_id'] for x in rows]
     return candidates
 
 # Return 0 if free
 # Return 1 if we interviewer have other event
 # Return 2 if we candidate have other event
-def validationHelper(candidates, start_time, end_time, interviewer_id):    
+def validationHelper(candidates, start_time, end_time, interviewer_id, update = 0, interview_id = ""):    
     cmd1 = "SELECT interview_id, interviewer_id FROM interview WHERE NOT(end_time < {}  OR  {} < start_time)".format(start_time, end_time)
     cmd2 = "SELECT count(DISTINCT(candidate_id)) FROM candidates  as T1 WHERE T1.interview_id = ( SELECT T2.interview_id FROM interview as T2 WHERE NOT(T2.end_time < {}  OR  {} < T2.start_time))".format(start_time, end_time)
-    print(cmd1)
-    print(cmd2)
+    if update:
+        cmd1 = "SELECT interview_id, interviewer_id FROM interview WHERE interview_id <> {} AND NOT(end_time < {}  OR  {} < start_time)".format(interview_id, start_time, end_time)
+        cmd2 = "SELECT count(DISTINCT(candidate_id)) FROM candidates  as T1 WHERE T1.interview_id = ( SELECT T2.interview_id FROM interview as T2 WHERE interview_id <> {} AND NOT(T2.end_time < {}  OR  {} < T2.start_time))".format(interview_id, start_time, end_time)
     ans = 3
     try:
         with sql.connect('database.db') as con:
@@ -55,14 +55,12 @@ def validationHelper(candidates, start_time, end_time, interviewer_id):
             curr = con.cursor()
             curr.execute(cmd1)
             rows = curr.fetchall()
-            print(rows)
             for row in rows:
                 if row['interviewer_id'] == interviewer_id:
                     ans = 1
                     raise Exception("1")
             curr.execute(cmd2)
             candidateBusy = curr.fetchall()[0]['count(DISTINCT(candidate_id))']
-            print(candidateBusy)
             if candidateBusy > 0:
                 ans = 2
                 raise Exception("2")
@@ -85,20 +83,19 @@ def home():
 
 @app.route('/enternew')
 def addInterview():
-    user_id = getting_user_id()
-    return render_template('interviewForm.html', msg = "", user_id = user_id)
+    email_id = getting_user_id()
+    return render_template('interviewForm.html', msg = "", user_id = email_id)
 
 @app.route('/submitForm', methods = ['POST', 'GET'])
 def submitForm():
     if request.method == 'POST':
         msg = ""
         try:
-            interviewer_id = int(request.form['interviewer_id'])
+            interviewer_id = str(request.form['interviewer_id'])
             start_time = int(request.form['start_time'])
             end_time = int(request.form['end_time'])
             candidates = request.form.getlist('candidate_id')
             valid = validationHelper(candidates, start_time, end_time, interviewer_id)
-            print(valid)
             if valid == 1:
                 msg = "interviewer have other event"
                 print(msg)
@@ -113,9 +110,8 @@ def submitForm():
                     cur = con.cursor()
                     print(interviewer_id, start_time, end_time, candidates)
                     cur.execute("INSERT INTO interview (start_time, end_time, interviewer_id) VALUES (?, ?, ?)", (start_time, end_time, interviewer_id))
-                    interview_id = int(cur.lastrowid)
+                    interview_id = cur.lastrowid
                     for candidate_id in candidates:
-                        candidate_id = int(candidate_id)
                         cur.execute("INSERT INTO candidates (interview_id, candidate_id) VALUES (?, ?)", (interview_id, candidate_id))
                     con.commit()
                     msg="Interview Details successfully added"
@@ -124,7 +120,7 @@ def submitForm():
             msg = "Error while updating interview details"
             print(ex)
         finally:
-            return render_template("interviewForm.html", msg = msg)
+            return render_template("home.html", msg = msg)
             con.close()
 
 @app.route('/viewList')
@@ -141,6 +137,59 @@ def viewList():
         email_id = '  ||  '.join(email_id)
         rows[i]['email'] = email_id
     return render_template('list.html', rows = rows)
+
+@app.route('/edit/<interview_id>')
+def edit(interview_id):
+    user_id = getting_user_id()
+    cmd = "SELECT * FROM interview WHERE interview_id = {}".format(interview_id)
+    try:
+        with sql.connect('database.db') as con:
+            con.row_factory = dict_factory
+            curr = con.cursor()
+            curr.execute(cmd)
+            rows = curr.fetchall()[0]
+            print(rows)
+    except Exception as ex:
+            con.rollback()
+            print(ex)
+    finally:
+        return render_template('edit.html', rows = rows, user_id = user_id)
+        con.close()
+
+@app.route('/editForm', methods = ['POST'])
+def editForm():
+    interviewer_id = request.form['interviewer_id']
+    start_time = request.form['start_time']
+    end_time = request.form['end_time']
+    interview_id = request.form['interview_id']
+    candidates = candidates_from_interview_id(interview_id)    
+    try:
+        valid = validationHelper(candidates, start_time, end_time, interviewer_id, 1, interview_id)
+        if valid == 1:
+            msg = "interviewer have other event"
+            print(msg)
+        elif valid == 2:
+            msg = "candidate have other event"
+            print(msg)
+        elif valid == 3:
+            msg = "database error"
+            print(msg)
+        else:
+            cmd = "UPDATE interview SET start_time = {}, end_time = {}, interviewer_id = '{}' WHERE interview_id = {};".format(start_time, end_time, interviewer_id, interview_id)
+            with sql.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute(cmd)
+                con.commit()
+                msg="Interview Details successfully updated"
+
+    except Exception as ex:
+            con.rollback()
+            msg = "Error while updating interview details"
+            print(ex)
+    finally:
+        return render_template("home.html", msg = msg)
+        con.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
